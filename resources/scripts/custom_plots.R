084a4c42288852e1054288c9abe15e5932c2b4ee
Lioche <lionel.lamy@student.uclouvain.be>
2021-04-19 20:37:08 +0200

    Data transformations and sarima_model_selection.R

--- resources/scripts/custom_plots.R
+++ resources/scripts/custom_plots.R
@@ -1,6 +1,7 @@
+# ---------------------------------------
 # Override the stats S3 method in order to customize the title
-plot.decompose.ts <- function(time.series, type = c("additive", "multiplicative"), ...) {
-  x <- decompose(time.series, type)
+plot.decompose.ts <- function(time.serie, type = c("additive", "multiplicative"), main="", ...) {
+  x <- decompose(time.serie, type)
   xx <- x$x
   if (is.null(xx)) {
     xx <- with(x, if (type == "additive") {
@@ -9,40 +10,114 @@ plot.decompose.ts <- function(time.series, type = c("additive", "multiplicative"
       random * trend * seasonal
     })
   }
-  plot(cbind(Observed = xx, Trend = x$trend, Seasonal = x$seasonal, Random = x$random), ...)
+  plot(cbind(Observed = xx, Trend = x$trend, Seasonal = x$seasonal, Random = x$random), main=main, ...)
 }
 
-
-plot.variance.ts <- function(time.series, title="", std = T, ...) {
+# ---------------------------------------
+plot.variance.ts <- function(time.serie, title = "", std = T, ...) {
   ts.attr <- attributes(gas)$tsp
   ts.start <- ts.attr[1]
   ts.end <- ts.attr[2]
   ts.freq <- ts.attr[3]
-  
-  variances <- sapply(seq(from = 1, to = abs(ts.end - ts.start)), function(i) var(time.series[ts.freq * (i - 1) + (1:ts.freq)]))
+
+  variances <- sapply(seq(from = 1, to = abs(ts.end - ts.start)), function(i) var(time.serie[ts.freq * (i - 1) + (1:ts.freq)]))
   if (std) {
     variances <- sqrt(variances)
   }
-  plot(variances, type = "b", xlab = "Year", ylab = ifelse(std, "Standard deviation", "Variance"), main=title, ...)
-  # abline(h = quantile(variances, .25), col = 4, lty = 2)
-  # abline(h = quantile(variances, .75), col = 4, lty = 2)
-  # legend("topleft", legend = c("1st quantile", "3rd quantile"), col = 4, lty = 2, cex = .8)
+  plot(variances, type = "b", xlab = "Year", ylab = ifelse(std, "Standard deviation", "Variance"), main = title, ...)
+  abline(h = quantile(variances, .25), col = 4, lty = 2)
+  abline(h = quantile(variances, .75), col = 4, lty = 2)
+  legend("topleft", legend = c("1st quantile", "3rd quantile"), col = 4, lty = 2, cex = .8)
 }
 
-plot.superposed.ts = function(time.series, title="Superposed", ...){
+# ---------------------------------------
+plot.superposed.ts <- function(time.serie, title = "Superposed", dashed_thick_from = c(8, 8), ...) {
   ts.attr <- attributes(gas)$tsp
   ts.start <- ts.attr[1]
   ts.end <- ts.attr[2]
   ts.freq <- ts.attr[3]
-  
-  plot(time.series[1:ts.freq],
-       type = "l", main = title,
-       ylim = c(min(time.series), max(time.series)), ...
+
+  colors <- rainbow(abs(ts.end - ts.start))
+
+  plot(time.serie[1:ts.freq],
+    type = "l", main = title,
+    ylim = c(min(time.serie), max(time.serie)), ...
   )
-  
+
   for (i in 2:abs(ts.end - ts.start)) {
-    lines(time.series[ts.freq * (i - 1) + (1:ts.freq)], col = i, lty = ifelse(i > 8, 2, 1), lwd = ifelse(i > 8, 2, 1))
+    lines(time.serie[ts.freq * (i - 1) + (1:ts.freq)], col = colors[i - 1], lty = ifelse(i > dashed_thick_from[1], 2, 1), lwd = ifelse(i > dashed_thick_from[2], 2, 1))
+  }
+
+  legend("topleft", legend = ts.start:(ts.end-1), lty = c(rep(1, dashed_thick_from[1]), rep(2, abs(ts.end - ts.start) - dashed_thick_from[1])), col = c(1, colors), cex = 0.5, bg = "white")
+}
+
+# ---------------------------------------
+plot.acf.pacf = function(time.serie, lag.max = 50, linked_by_line=T){
+  
+  # Retrieve the ACF/PACF
+  ts.acf = acf(x=time.serie, lag=lag.max, plot = F)
+  ts.pacf = pacf(x=time.serie, lag=lag.max, plot=F)
+  
+  # Only keep the rounded lags
+  # Improve the visualization
+  ts.seq = seq(from=frequency(time.serie), to=lag.max, by=frequency(time.serie))
+  
+  ts.acf$acf = array(ts.acf$acf[c(1,ts.seq+1)], dim=c(length(ts.seq), 1, 1))
+  ts.acf$lag = array(ts.acf$lag[c(1,ts.seq+1)], dim=c(length(ts.seq), 1, 1))
+  
+  ts.pacf$acf = array(ts.pacf$acf[c(1,ts.seq)], dim=c(length(ts.seq), 1, 1))
+  ts.pacf$lag = array(ts.pacf$lag[c(1, ts.seq)], dim=c(length(ts.seq), 1, 1))
+  
+  # Plot the modified ones
+  # Add lines to improve further the visualization
+  
+  plot(ts.acf, ylim=c(min(ts.acf$acf)-0.2, min(max(ts.acf$acf+0.3), 1)), main="ACF", lwd=2)
+  if(linked_by_line){lines(ts.acf$lag, ts.acf$acf, type="b", col=rgb(0,0,0,.7))}
+  
+  plot(ts.pacf, ylim=c(min(ts.pacf$acf)-0.2, min(max(ts.pacf$acf+0.3), 1)), main="PACF", lwd=2)
+  if(linked_by_line){lines(ts.pacf$lag, ts.pacf$acf, type="b", col=rgb(0,0,0,.7))}
+}
+
+# ---------------------------------------
+plot.n.ahead.predictions <- function(time.serie, model, n = 10, before = 48, holtwinters=F) {
+  len <- length(time.serie)
+  
+  if (holtwinters){
+    fore = predict(HoltWinters(time.serie, beta = FALSE), n.ahead = n, type="response", prediction.interval=T, level=0.95)
+  }else{
+    fore <- predict(model, n.ahead = n, type="response")
   }
   
-  legend("topleft", legend = ts.start:(ts.end - 1), lty = c(rep(1, 8), rep(2, abs(ts.end - ts.start) - 8)), col = 1:abs(ts.end - ts.start), cex = 0.5, bg = "white")
+  before = min(before, length(time.serie))
+  ts.freq = frequency(time.serie)
+  
+  draw = ts(c(tail(time.serie, before), ifelse(isTRUE(holtwinters), fore[1,1], fore$pred[1])), start=end(time.serie)[1]-(before/ts.freq)+1, frequency = ts.freq) 
+  
+  plot(draw, type="l", main=paste0("Time serie and prediction (", n, "-ahead)", ifelse(holtwinters, "\nHolt-winter's method", "")), ylim=c(min(draw)-0.1*min(draw), max(draw)+0.1*max(draw)),
+       xlab = "Time", ylab = "Y", xlim=c(start(draw)[1], end(time.serie)[1]+ceiling(n/ts.freq)+0.1*(n/ts.freq)))
+  
+  
+  if(holtwinters){
+    lines(fore[, "fit"], col=2)
+    lines(fore[, "upr"], lty="dashed", col=4)
+    lines(fore[, "lwr"], lty="dashed", col=4)
+  }else{
+    lines(fore$pred, col=2)
+    lines(fore$pred + 1.96*fore$se, lty="dashed", col=4)
+    lines(fore$pred - 1.96*fore$se, lty="dashed", col=4)
+  }
+  
+  legend("topleft", legend = c("Observations", "Prediction", "95% Confidence"), col = c(1, 2, 4), lty = c(1, 1, 2), cex = .8, lwd = 2)
+}
+
+# ---------------------------------------
+plot.last.n.predictions <- function(time.serie, order, seasonal=list(order = c(0L, 0L, 0L), period=NA), n = 10, main="Last n predictions") {
+  ts.ahead <- OneAhead(time.serie, order, seasonal)
+  
+  len <- length(time.serie)
+  n <- min(n, 0.8 * len)
+  
+  plot(time.serie[I(len - n):len], type = "l", main = main, ylab = "Y", xlab = "Time", lwd = 1)
+  lines(ts(ts.ahead$tspred[I(len - n):len], start = 0, end = n), col = 2, lty = 2, lwd = 1)
+  legend("topleft", legend = c("Observations", "Simulated"), col = c(1, 2), lty = c(1, 2), cex = .8, lwd = 2)
 }
---
+++ resources/scripts/sarima_model_selection.R
@@ -0,0 +1,80 @@
+#' 
+#' @concept Comp.Sarima redesigned
+#' @author Lionel Lamy
+#' 
+#' @description 
+#' Comparison using the AIC criterion of different S-ARIMA models
+#' fitted to a time series with trend and seasonality removal.
+#' 
+#' @details 
+#' Returns the model with the lowest AIC.
+#' Displays the percent.best% models in the console.
+#' Given info are AIC, AICR (relative), BIC and the nb of parameters.
+#' 
+#' @note
+#' Be careful not to set the maximum values too high.
+#' The data must be the original one, not differentiated.
+#' 
+#' @method sarima_model_selection(data, max.pq, max.PQ, d, D, percent.best)
+#' 
+#' @param data the (univariate) time series. Must be class of ts().
+#' @param max.pq vector of length 2, maximum orders for p and q.
+#' @param max.PQ vector of length 2, maximum orders for P and Q.
+#' @param d number of diff of lag 1 for trend elimination.
+#' @param D number of diff of lag 'season' for seasonality elimination.
+#' @param percent.best the percentage of best models returned.
+#' 
+#' @return the model with the lowest AIC.
+
+sarima_model_selection <- function(data, max.pq = c(1, 1), max.PQ = c(1, 1), d = 1, D = 1, percent.best = .1) {
+
+  if (!is(data, "ts")) {
+    warning("The argument 'data' must be a time series object!", immediate. = T)
+    return(NA)
+  }
+
+  season <- frequency(data)
+  # Expand grid is equivalent to nested loops
+  loop <- expand.grid(p = 0:max.pq[1], q = 0:max.pq[2], P = 0:max.PQ[1], Q = 0:max.PQ[2])
+
+  # For each (pq)x(PQ) possibility
+  # Returns the AIC, BIC and the model
+  results <- sapply(seq(1, nrow(loop)), function(i) {
+    arr <- loop[i, ]
+    temp <- arima(data, order = c(arr$p, d, arr$q), seasonal = list(order = c(arr$P, D, arr$Q), period = season))
+    list(AIC = AIC(temp), BIC = BIC(temp), ARMA = temp$arma)
+  })
+
+  # Unpack the results
+  AIC.table <- round(unlist(results[1, ]), 3)
+  BIC.table <- round(unlist(results[2, ]), 3)
+
+  # Retrieve the best one (for comparative purposes)
+  AIC.best <- which(AIC.table == min(AIC.table), T)
+  AICR.table <- AIC.table - AIC.table[AIC.best]
+
+  # Get the percent.best% results
+  AICQ <- quantile(AICR.table, percent.best, na.rm = T)
+  AIC.top <- which(AICR.table <= AICQ)
+
+  # Print them in a readable way
+  cat("TOP ", percent.best * 100, "% AIC\n", sep = "")
+  cat("MODEL : (p,d,q)x(P,D,Q)[s]\n\n")
+  sapply(AIC.top, function(i) {
+    sarima <- unlist(results[3, i])
+    m1 <- paste0(sarima[c(1, 6, 2)], collapse = ",") # (p,d,q)
+    m2 <- paste0(sarima[c(3, 7, 4)], collapse = ",") # (P,D,Q)
+
+    cat("(", m1, ")x(", m2, ")[", sarima[5], "]",
+      " || AIC: ", format(AIC.table[i], nsmall = 3),
+      " | AICR: ", format(AICR.table[i], nsmall = 3),
+      " | BIC: ", format(BIC.table[i], nsmall = 3),
+      " || P: ", sum(sarima[1:4]), "\n",
+      sep = ""
+    )
+  })
+
+  # Return the best model, based on AIC
+  best <- unlist(results[3, AIC.best])
+  arima(data, order = c(best[1], d, best[2]), seasonal = list(order = c(best[3], D, best[4]), period = season))
+}
